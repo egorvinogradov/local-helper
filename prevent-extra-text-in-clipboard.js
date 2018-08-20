d960db8895b11013358554385b528f4a8071815c
Egor Vinogradov <egor.vinogradov@corp.mail.ru>
2018-08-20 15:58:07 +0300

    Fix clipboard script

---
+++ prevent-extra-text-in-clipboard.js
@@ -0,0 +1,24 @@
+/**
+ * Prevents annoying extra text from being added to clipboard while copying text on various websites
+ * Works on:
+ * - www.rbc.ru
+ * - notion.so
+ */
+
+
+const DOMAIN_HANDLERS = {
+  'www.rbc.ru': (text) => {
+    return text.split(/\n\nПодробнее на РБК:/m)[0];
+  }
+};
+
+document.addEventListener('copy', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  let text = window.getSelection().toString();
+
+  if (DOMAIN_HANDLERS[location.hostname]) {
+    text = DOMAIN_HANDLERS[location.hostname](text, e);
+  }
+  navigator.clipboard.writeText(text);
+}, false);
