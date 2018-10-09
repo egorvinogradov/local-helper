45b9a070aafb1de8a817e37e4b8308c44f46e174
Egor Vinogradov <egor.vinogradov@corp.mail.ru>
2018-10-09 15:28:16 +0300

    Redirect to google if accidentally typed "п"

--- background.js
+++ background.js
@@ -6,3 +6,10 @@ chrome.runtime.onMessage.addListener(function(request) {
     });
   }
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  var tabUrl = decodeURIComponent(changeInfo.url);
+  if (tabUrl === 'https://www.google.com/search?q=п' && changeInfo.status === 'loading') {
+    chrome.tabs.update(tabId, {url: 'https://www.google.com'});
+  }
+});
