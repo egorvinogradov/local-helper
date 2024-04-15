b3aac3ea642276d89daddd968560a8a8cf39a5e1
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2024-04-15 08:24:12 +0600

    Improve TGStats parser

--- tgstat-extension.js
+++ tgstat-extension.js
@@ -1,3 +1,5 @@
+const SKIP_TYPES = ['script', '.apexcharts-tooltip-text-y-label'];
+
 function getElementsByContent(targetString, parent = document) {
   const allElements = parent.querySelectorAll('body *');
   const directlyContainingElements = [];
@@ -23,24 +25,47 @@ function excludeBySelectors(elements, selectors) {
 }
 
 
-function parseChatPage(dom){
+function parseNum(str){
+  str = (str || '').replace(/\s+/g, '').trim();
+  return str.endsWith('k')
+    ? +str.replace(/k$/, '') * 1000
+    : +str;
+}
+
+
+function parseChatPage(dom = document){
+  let dau = 6666666;
   let wau = 6666666;
+  let mau = 6666666;
   let usersOnline = 6666666;
+  let messagesDay = 6666666;
   let messagesWeek = 6666666;
+  let messagesMonth = 6666666;
 
   try {
-    const SKIP_TYPES = ['script', '.apexcharts-tooltip-text-y-label'];
     const wauEl = excludeBySelectors(getElementsByContent('WAU', dom), SKIP_TYPES)[0];
-    wau = +wauEl?.parentElement.innerText.replace(/wau/i, '').trim() || 0;
+    wau = parseNum(wauEl?.parentElement.innerText.replace(/wau/i, '')) || 0;
+
+    const mauEl = excludeBySelectors(getElementsByContent('MAU', dom), SKIP_TYPES)[0];
+    mau = parseNum(mauEl?.parentElement?.parentElement.innerText.replace(/mau/i, '')) || 0;
+
+    const dauEl = excludeBySelectors(getElementsByContent('DAU', dom), SKIP_TYPES)[0];
+    dau = parseNum(dauEl?.parentElement?.parentElement.innerText.replace(/dau/i, '')) || 0;
 
     const usersOnlineEl = excludeBySelectors(getElementsByContent('участники онлайн', dom), SKIP_TYPES)[0];
-    usersOnline = +usersOnlineEl?.parentElement.innerText.split(/\n/)[0].replace(/\s+/g, '') || 0;
+    usersOnline = parseNum(usersOnlineEl?.parentElement.innerText.split(/\n/)[0]) || 0;
 
     const messagesEl = excludeBySelectors(getElementsByContent('сообщения', dom), SKIP_TYPES)[0];
 
     if (messagesEl) {
+      const messageDayEl = getElementsByContent('вчера', messagesEl.parentElement)[0];
+      messagesDay = parseNum(messageDayEl?.parentElement.innerText.split(/\s/)[0]) || 0;
+
       const messageWeekEl = getElementsByContent('за неделю', messagesEl.parentElement)[0];
-      messagesWeek = +messageWeekEl?.parentElement.innerText.split(/\s/)[0] || 0;
+      messagesWeek = parseNum(messageWeekEl?.parentElement.innerText.split(/\s/)[0]) || 0;
+
+      const messageMonthEl = getElementsByContent('за месяц', messagesEl.parentElement)[0];
+      messagesMonth = parseNum(messageMonthEl?.parentElement.innerText.split(/\s/)[0]) || 0;
     }
   }
   catch(e){
@@ -50,17 +75,17 @@ function parseChatPage(dom){
 
   return {
     wau,
+    dau,
+    mau,
     usersOnline,
+    messagesDay,
     messagesWeek,
+    messagesMonth,
   };
 }
 
 
-function parseChannelPage(dom){
-  if (!dom) {
-    return null;
-  }
-
+function parseChannelPage(dom = document){
   let errTotal = 6666666;
   let err24h = 6666666;
   let er = 6666666;
@@ -69,8 +94,6 @@ function parseChannelPage(dom){
   let reach48 = 6666666;
 
   try {
-    const SKIP_TYPES = ['script', '.apexcharts-tooltip-text-y-label'];
-
     const errTotalEl = excludeBySelectors(getElementsByContent('подписчиков читают посты канала', dom), SKIP_TYPES)[0];
     errTotal = +errTotalEl?.parentElement.innerText.trim().split(/\n/)[0].replace('%', '') || 0;
 
@@ -91,9 +114,9 @@ function parseChannelPage(dom){
       reach48El = getElementsByContent('48 часов', reachEl)[0];
     }
 
-    reach12 = +reach12El?.parentElement.innerText.trim().split(/\t|\n/)[0] || 0;
-    reach24 = +reach24El?.parentElement.innerText.trim().split(/\t|\n/)[0] || 0;
-    reach48 = +reach48El?.parentElement.innerText.trim().split(/\t|\n/)[0] || 0;
+    reach12 = parseNum(reach12El?.parentElement.innerText.trim().split(/\t|\n/)[0]) || 0;
+    reach24 = parseNum(reach24El?.parentElement.innerText.trim().split(/\t|\n/)[0]) || 0;
+    reach48 = parseNum(reach48El?.parentElement.innerText.trim().split(/\t|\n/)[0]) || 0;
   }
   catch(e){
     window.__error = e;
@@ -101,9 +124,9 @@ function parseChannelPage(dom){
   }
 
   return {
-    errTotal,
-    err24h,
-    er,
+    errTotal: errTotal / 1,
+    err24h: err24h / 1,
+    er: er / 1,
     reach12,
     reach24,
     reach48,
@@ -117,18 +140,29 @@ function saveDataLocally(data){
 
 
 function __parse(){
-  const type = location.pathname.includes('/channel/') ? 'channel' : 'chat';
+  const type = location.pathname.includes('/channel/')
+    ? 'channel'
+    : location.pathname.includes('/chat/')
+      ? 'chat'
+      : null;
+
+  if (!type) {
+    return;
+  }
+
   const handleBase = location.pathname.split(/\/channel\/|\/chat\//)[1].split('/')[0];
   const handle = handleBase.startsWith('@') ? handleBase.replace(/^@/, '') : '+' + handleBase;
 
   let data = {};
 
   if (type === 'channel') {
-    data = parseChannelPage(document);
+    data = parseChannelPage();
   }
   else {
-    data = parseChatPage(document);
+    data = parseChatPage();
   }
+
+  data.type = type;
   data.handle = handle;
   data.tgStatLink = location.href;
 
@@ -138,3 +172,173 @@ function __parse(){
 
 
 __parse();
+
+
+
+/********************/
+
+/*
+ * EXTRACTING SAVED DATA
+ */
+
+function copyStringFallback(str) {
+  const textArea = document.createElement('textarea');
+  textArea.value = str;
+  textArea.style.position = 'fixed';
+  textArea.style.opacity = '0';
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    const successful = document.execCommand('copy');
+    const msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Unable to copy', err);
+  }
+  document.body.removeChild(textArea);
+}
+
+
+function extractStoredTGStats(tgUrlsRaw){
+  const allStorageKeys = Object.keys(localStorage).filter(key => key.startsWith('TGStatsParser_'));
+  const hostKey = 'TGSTAT_' + location.hostname.replace(/\./g, '_').toUpperCase();
+
+  const allStats = tgUrlsRaw
+    .split(/\n/)
+    .map(url => {
+      const handle = url.replace('https://t.me/', '').replace(/\/$/, '');
+      const storageKey = allStorageKeys.find(key => {
+        return key.replace('TGStatsParser_', '').toLowerCase() === handle.toLowerCase();
+      });
+
+      let stats = null;
+      try {
+        stats = JSON.parse(localStorage.getItem(storageKey));
+      }
+      catch(e) {
+        console.warn('TGStat Aggregation Error:', e);
+      }
+      return stats;
+    });
+
+  console.warn(hostKey, allStats);
+  window[hostKey] = allStats;
+  return allStats;
+}
+
+
+async function handleStoredTgStatsExtraction(){
+  let tgUrls = '';
+  let combinedStat = [];
+
+  const clipboardText = await navigator.clipboard.readText();
+
+  if (clipboardText.startsWith('https://t.me/')) {
+    console.error('🛑 NO PREV TG STATS, URL ONLY', { TG_URLS: clipboardText });
+    const currentTgStat = extractStoredTGStats(clipboardText);
+    tgUrls = clipboardText;
+    combinedStat = currentTgStat;
+  }
+  else if (clipboardText.startsWith('window.ALL_TGSTAT')) {
+    eval(clipboardText);
+    console.error('✅ HAS PREV TG STATS, COMBINING', { ALL_TGSTAT: window.ALL_TGSTAT, TG_URLS: window.TG_URLS });
+    const currentTgStat = extractStoredTGStats(window.TG_URLS);
+    tgUrls = window.TG_URLS;
+    combinedStat = window.ALL_TGSTAT.map((item, index) => {
+      if (item) {
+        return item;
+      }
+      return currentTgStat[index] || null;
+    });
+  }
+  else {
+    alert(`Can't detect. Copy URL col from Airtable.`);
+  }
+
+  if (tgUrls && combinedStat.length) {
+    window.TG_URLS = tgUrls;
+    window.ALL_TGSTAT = combinedStat;
+    localStorage.__ALL_TGSTAT = JSON.stringify(combinedStat);
+
+    copyStringFallback(`window.ALL_TGSTAT = ${JSON.stringify(combinedStat)}; window.TG_URLS = \`${tgUrls}\``);
+
+    const successMessage = `Success. ${combinedStat.filter(val => val).length} non-empty out of ${combinedStat.length}.`
+    alert(successMessage);
+  }
+}
+
+
+function copyTgStatsToAirtable(){
+  let allStats = [];
+  try {
+    allStats = JSON.parse(localStorage.__ALL_TGSTAT);
+  }
+  catch(e){}
+  if (!allStats.length) {
+    alert(`Can't find localStorage.__ALL_TGSTAT`);
+    return;
+  }
+
+  const airtableContent = allStats.map(row => {
+    return [
+      row.er,
+      row.errTotal,
+      row.err24h,
+      row.reach12,
+      row.reach24,
+      row.reach48,
+      row.dau,
+      row.wau,
+      row.mau,
+      row.messagesDay,
+      row.messagesWeek,
+      row.messagesMonth,
+      row.usersOnline,
+    ]
+    .map(cell => `"${cell}"`).join('\t');
+  }).join('\n');
+  copyStringFallback(airtableContent);
+  alert('Copied Airtable content');
+}
+
+
+// Exposing functions to bookmarklet scope from extension scope
+localStorage.__handleStoredTgStatsExtraction = handleStoredTgStatsExtraction.toString();
+localStorage.__copyTgStatsToAirtable = copyTgStatsToAirtable.toString();
+localStorage.__extractStoredTGStats = extractStoredTGStats.toString();
+localStorage.__copyStringFallback = copyStringFallback.toString();
+
+
+
+
+
+
+
+
+/* Bookmarklet code collect *
+
+javascript:(() => {
+  eval(localStorage.__handleStoredTgStatsExtraction);
+  eval(localStorage.__extractStoredTGStats);
+  eval(localStorage.__copyStringFallback);
+  handleStoredTgStatsExtraction();
+})();
+
+*/
+
+
+/* Bookmarklet code copy *
+
+javascript:(() => {
+  eval(localStorage.__copyTgStatsToAirtable);
+  eval(localStorage.__copyStringFallback);
+  copyTgStatsToAirtable();
+})();
+
+*/
+
+
+
+
